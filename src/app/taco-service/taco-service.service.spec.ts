d18f1e3cd410fa783f54f0ab6f663f47a5cb170c
Seamus Riordan <seamus@riordan.io>
2021-03-21 09:28:01 -0500

    Move correct taco up

--- src/app/taco-service/taco-service.service.spec.ts
+++ src/app/taco-service/taco-service.service.spec.ts
@@ -2,7 +2,7 @@ import {TestBed} from '@angular/core/testing';
 
 import {TacoServiceService} from './taco-service.service';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import {Taco} from './taco';
+import {Taco} from '../taco';
 
 describe('TacoServiceService', () => {
   let tacoService: TacoServiceService;
@@ -22,20 +22,6 @@ describe('TacoServiceService', () => {
     expect(tacoService).toBeTruthy();
   });
 
-  it('gets right number of tacos with one taco', () => {
-    const mockTacoTypes = ['Boring'];
-
-    tacoService.getTacos().subscribe(
-      tacos => {
-        expect(tacos.length).toEqual(mockTacoTypes.length);
-      }
-    );
-
-    const request = httpTestingController.expectOne(url);
-    request.flush({types: mockTacoTypes});
-  });
-
-
   it('gets correct taco', () => {
     const mockTacoTypes = ['Fancy'];
     let foundTacos: Taco [] = [];
@@ -51,6 +37,19 @@ describe('TacoServiceService', () => {
     expect(foundTacos[0].type).toEqual('Fancy');
   });
 
+  it('gets right number of tacos with one taco', () => {
+    const mockTacoTypes = ['Boring'];
+
+    tacoService.getTacos().subscribe(
+      tacos => {
+        expect(tacos.length).toEqual(mockTacoTypes.length);
+      }
+    );
+
+    const request = httpTestingController.expectOne(url);
+    request.flush({types: mockTacoTypes});
+  });
+
   it('gets right number of tacos with two tacos', async () => {
     const mockTacoTypes = ['Lovely', 'Fortuitous'];
     let foundLength = 0;
@@ -64,7 +63,8 @@ describe('TacoServiceService', () => {
     const request = httpTestingController.expectOne(url);
     request.flush({types: mockTacoTypes});
 
-    await setInterval(() => {}, 500);
+    await setInterval(() => {
+    }, 500);
 
     expect(foundLength).toEqual(mockTacoTypes.length);
   });
--- src/app/taco-service/taco-service.service.ts
+++ src/app/taco-service/taco-service.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
-import {Taco} from './taco';
+import {Taco} from '../taco';
 import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 
